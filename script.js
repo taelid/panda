11cebbbfb9f00af0ec02a6b7983cc2ac0aa255a6
taelid <tayyab.khalid7@hotmail.com>
2022-04-02 16:22:34 -0500

    added easter eggs

--- script.js
+++ script.js
@@ -4,31 +4,37 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const showClickyModal = document.querySelectorAll(".clicky");
+const thurst = document.querySelector("#thursty");
 
-// function to show modal
+// show modal
 const showModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 
-// function to close modal
+// close modal
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
 
-// function to show modal on click
+// show modal on click
 for (let i = 0; i < showClickyModal.length; i++) {
   showClickyModal[i].addEventListener("click", showModal);
 }
 
-// function to close modal on clicks
+// close modal on clicks
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
-// function to close modal on ESC
+// close modal on ESC
 document.addEventListener("keydown", function (event) {
   if (event.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
+});
+
+// boomer thurston
+thurst.addEventListener("click", function () {
+  document.getElementById("thursty").src = "/static/thurston3.jpg";
 });
